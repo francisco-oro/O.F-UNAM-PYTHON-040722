2b1cc3e9a6bc878789aadefe9faf512e3fa11c88
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-10-17 14:38:59 -0500

    Add files via upload

---
+++ PE1: Module 2/Unit 2/M2_U2_tasks.py
@@ -0,0 +1,13 @@
+﻿# Task 1
+# What type of literals are the following examples?
+# "Hola ", "007" -> string & string
+print(type("Hola"))
+print(type("007"))
+# Task 2
+# What kind of literals are the following four examples?
+# "1.5", 2.0, 528, False -> string, float, int, bool
+print(type("1.5"), type(2.0), type(528), type(False))
+# Task 3
+# What is the decimal value of the following binary number? -> 11
+1011
+print(0b1011)
