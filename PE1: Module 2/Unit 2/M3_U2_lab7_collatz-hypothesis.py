2d288e7b82ff96bc5b25a8fb7cc6ce900bf4ca2e
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-10-17 14:47:07 -0500

    Add files via upload

---
+++ PE1: Module 2/Unit 2/M3_U2_lab7_collatz-hypothesis.py
@@ -0,0 +1,11 @@
+﻿# Collatz Hypothesis
+c0 = int(input("Enter a number: "))
+counter = 0 
+while c0 > 1:
+    if c0 % 2 == 0:
+        c0 //= 2
+    else: 
+        c0 = 3 * c0 + 1
+    print(c0)
+    counter += 1
+print("steps:", counter)
