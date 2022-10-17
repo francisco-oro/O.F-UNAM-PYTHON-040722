4a6bd0a3092fd5ed0404f72662b8f8704020a96e
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-10-17 14:55:54 -0500

    Add files via upload

---
+++ PE1: Module 2/M3_U6_lab1_operating-with-lists-basic-concepts.py
@@ -0,0 +1,8 @@
+﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+newList = []
+for number in my_list: 
+    if number in newList:
+        continue
+    newList.append(number)
+print("The list with non-repeating elements:")
+print(newList)
