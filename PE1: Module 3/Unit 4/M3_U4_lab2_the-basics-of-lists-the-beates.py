48af7140ae7b4ba59b65c47ac323898650ba584b
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-10-18 15:13:02 -0500

    Add files via upload

---
+++ PE1: Module 3/Unit 4/M3_U4_lab2_the-basics-of-lists-the-beates.py
@@ -0,0 +1,16 @@
+﻿beatles = []
+
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+
+for i in range(2):
+    newMember = input("Give me the name of one of the remaining members: ")
+    beatles.append(newMember)
+del beatles[-2]
+del beatles[-1]
+beatles.insert(0, "Ringo Starr")
+
+
+# probando la longitud de la lista
+print("Los Fav", len(beatles))
