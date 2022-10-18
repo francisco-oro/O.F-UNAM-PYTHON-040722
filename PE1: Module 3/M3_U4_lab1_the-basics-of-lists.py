8522cabf6f226b497be6ce3088235c68c11b7f03
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-10-18 15:12:04 -0500

    Add files via upload

---
+++ PE1: Module 3/M3_U4_lab1_the-basics-of-lists.py
@@ -0,0 +1,7 @@
+﻿hat_list = [1, 2, 3, 4, 5]  # Esta es una lista existente de números ocultos en el sombrero.
+
+hat_list[2] = int(input("Give me a number: "))
+
+del hat_list[len(hat_list)-1]
+print("Current list length:", len(hat_list))
+print(hat_list)
