b89457c66697c44daa98c6ce4f43eb27817190b7
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-10-18 15:08:59 -0500

    Add files via upload

---
+++ PE1: Module 3/M3_U2_lab1_guess-the-secret-number.py
@@ -0,0 +1,23 @@
+﻿secret_number = 777
+givenNumber = 0
+print(
+"""
++==================================+
+| Welcome to my game, muggle!      |
+| enter an integer                 |
+| and guess the number I've        |
+| choosen for you.                 |
+| Then,                            |
+| What is the secret number?       |
++==================================+
+""")
+
+while givenNumber != secret_number:
+    try:
+        givenNumber = int(input("What is the secret number?: "))
+        if givenNumber != secret_number: 
+            print("Ha, ha! You're stuck in my loop!")
+    except:
+        print("Ha, ha! You're stuck in my loop!")
+
+print("Nicely done, muggle! You're free now")
