d76afa9b14e22bbb28dd25d8a22f643e5370ae55
Francisco Abimael Oro Estrada <faoe2003@gmail.com>
2022-10-18 13:10:03 -0500

    Add files via upload

---
+++ PE1: Module 2/M2_U6_lab1_simple-inputs-and-outputs.py
@@ -0,0 +1,10 @@
+﻿a = float(input("Input the first number: "))
+b = float(input("Input the second number: "))
+
+# Performing arithmetic operations
+print("sum: " + str(a + b))
+print("sub: " + str(a - b))
+print("mult: " + str(a * b))
+print("div: " + str(a / b))
+
+print("\n¡Eso es todo, amigos!")
